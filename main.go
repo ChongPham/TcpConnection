3018f533783bec6df0b9df5b3db504b1c11623a2
trongpham <trongpq091999>
2025-09-15 13:51:27 +0700

    using threadpool in tcp connection

--- main.go
+++ main.go
@@ -4,17 +4,89 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
+// element in the queue
+type Job struct {
+	conn net.Conn
+}
+
+// represent thread in the pool
+type Worker struct {
+	id      int
+	jobChan chan Job
+	wg      *sync.WaitGroup
+}
+
+// the pool
+type Pool struct {
+	jobQueue chan Job
+	workers  []*Worker
+	wg       *sync.WaitGroup
+}
+
+// Create new worker/thread
+func NewWorker(id int, jobChan chan Job, wg *sync.WaitGroup) *Worker {
+	return &Worker{
+		id:      id,
+		jobChan: jobChan,
+		wg:      wg,
+	}
+}
+
+// Start worker/thread
+func (w *Worker) Start() {
+	go func() {
+		for job := range w.jobChan {
+			log.Printf("worker %d is handing job from %s", w.id, job.conn.RemoteAddr())
+			handleConnection(job.conn)
+			w.wg.Done()
+		}
+	}()
+}
+
+// Create new pool
+func NewPool(numOfWorker int, limit int, wg *sync.WaitGroup) *Pool {
+	return &Pool{
+		jobQueue: make(chan Job, limit),
+		workers:  make([]*Worker, numOfWorker),
+		wg:       wg,
+	}
+}
+
+// Start take jobs
+func (p *Pool) Start() {
+	for i := 0; i < len(p.workers); i++ {
+		worker := NewWorker(i, p.jobQueue, p.wg)
+		p.workers[i] = worker
+		worker.Start()
+	}
+}
+
+// push job to queue
+func (p *Pool) AddJob(conn net.Conn) {
+	p.jobQueue <- Job{conn: conn}
+	p.wg.Add(1)
+}
+
+// Close Pool
+func (p *Pool) Shutdown() {
+	close(p.jobQueue) // báo cho worker dừng
+	p.wg.Wait()       // chờ tất cả worker xong việc
+}
+
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	log.Println(conn.RemoteAddr())
 	//read data from client
 	var buffer []byte = make([]byte, 1000)
 
 	for {
-		n, err := conn.Read(buffer)
+		_, err := conn.Read(buffer)
 		if err != nil {
 			if err == io.EOF {
 				log.Println("Client closed connection:", conn.RemoteAddr())
@@ -24,19 +96,12 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 
-		req := string(buffer[:n])
-		log.Println("Request from", conn.RemoteAddr(), ":\n", req)
-		//process sometihing
+		//process something
 		time.Sleep(time.Second * 2)
 
 		//rely to client
-		response := "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nhello world!"
-		m, err := conn.Write([]byte(response))
-		if err != nil {
-			log.Println("Write error:", err)
-			return
-		}
-		log.Println("Request tiep theo ne", m)
+		response := "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nhello world!\r\n/"
+		conn.Write([]byte(response))
 	}
 }
 
@@ -46,6 +111,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer listener.Close()
+
+	wg := &sync.WaitGroup{}
+	pool := NewPool(4, 10, wg)
+	pool.Start()
+
+	// Bắt tín hiệu Ctrl+C hoặc kill
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		log.Println("Shutting down server...")
+		listener.Close() // ngắt Accept()
+		pool.Shutdown()  // đóng pool
+		os.Exit(0)
+	}()
 
 	for {
 		// thiet lap dedicated connection channel
@@ -53,9 +135,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println(conn)
 
-		// create goroutine to handles
-		go handleConnection(conn)
+		// Add job to jobChan
+		pool.AddJob(conn)
 	}
 }
